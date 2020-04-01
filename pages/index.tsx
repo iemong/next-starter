3544808478703a45feb4e383791434f86791012d
iemong <kiyoshidainagon@gmail.com>
2020-04-01 19:08:28 +0900

    3rd party cookieどうなのか確認する

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,30 @@
 import * as React from 'react'
 import styled from '@emotion/styled'
 
-const Home = () => <Title>Hello, world!</Title>
+type DocumentTypeEx = Document & {
+    hasStorageAccess: () => Promise<any>
+}
+
+const Home = () => {
+    const [access, setAccess] = React.useState()
+    React.useEffect(() => {
+        if ('hasStorageAccess' in document) {
+            const promise = (document as DocumentTypeEx).hasStorageAccess()
+            promise.then(
+                (hasAccess: boolean) => {
+                    console.log(hasAccess)
+                    setAccess(JSON.stringify(hasAccess))
+                },
+                (reason: any) => {
+                    console.log(reason)
+                    setAccess(JSON.stringify(reason))
+                },
+            )
+        }
+    }, [])
+
+    return <Title>{access}</Title>
+}
 
 export default Home
 
